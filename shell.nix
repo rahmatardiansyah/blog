f369447adb08fb1cf60753bec634d9ce41c29a76
Rahmat Ardiansyah <rahmatardiansyah147@gmail.com>
2023-08-01 22:10:05 +0700

    add nix shell

--- shell.nix
+++ shell.nix
@@ -4,9 +4,7 @@ let
     pkgs = import (builtins.fetchTarball {
         url = "https://github.com/NixOS/nixpkgs/archive/d86bcbb415938888e7f606d55c52689aec127f43.tar.gz";
     }) {};
-
-    myPkg = pkgs.hugo;
 in
 mkShell {
-    buildInputs = [ myPkg ];
+    buildInputs = [ pkgs.hugo ];
 }
